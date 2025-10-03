c788c90969ef49b88628db57dd2b646649ab37f3
JSartini <jsartin1@jhu.edu>
2025-10-03 11:27:24 -0400

    Fix issue with second derivative calculation for Splinet basis

--- Gen_Funcs/Bases.R
+++ Gen_Funcs/Bases.R
@@ -106,8 +106,7 @@ Splinet_bases <- function(Q){
 Splinet_d2 <- function(Q, cInt, cSlo){
   
   B_f = splinet(knots = seq(0, 1, length.out = Q+2), norm = T)
-  first_deriv = deriva(B_f$os)
-  second_deriv = deriva(B_f$os)
+  second_deriv = deriva(deriva(B_f$os))
   
   derivs = map(1:(Q-2), function(q){
     func <- function(x){
